0aa2bd145aef8988bb3d10fed6a89d00937d4858
deniallugo <deniallugo@gmail.com>
2017-11-29 19:29:52 +1000

    delete old code

--- bot/modules/main_module.py
+++ bot/modules/main_module.py
@@ -7,9 +7,5 @@ message_scenario = MessageScenario()
 main_module = Module("main_module", "start",
                      scenarios=[message_scenario, callback_query_scenario])
 
-from bot.modules import start_module
-# from bot.modules import repay_module
-# from bot.modules import bonus_list_module
-# from bot.modules import company_module
-# from bot.modules import user_bonus_module
-# from bot.modules import feedback_module
+__all__ = ["start_module"]
+
